351b1545becb9978145d4b61d85ef73fecfc9b58
toymaj <sermed512@gmail.com>
2023-08-06 22:21:57 +0300

    Add missing import

--- likeinterface/methods/__init__.py
+++ likeinterface/methods/__init__.py
@@ -1,10 +1,16 @@
-from .auth import GetAuthorizationInformationMethod, RootAuthMethod, SignInMethod
+from .auth import (
+    GetAuthorizationInformationMethod,
+    GetUserInformationMethod,
+    RootAuthMethod,
+    SignInMethod,
+)
 from .base import LikeType, Method, Request, Response
 from .like import EvaluatorMethod, RootLikeMethod
 
 __all__ = (
     "EvaluatorMethod",
     "GetAuthorizationInformationMethod",
+    "GetUserInformationMethod",
     "RootAuthMethod",
     "LikeType",
     "Method",
