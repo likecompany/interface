6bbcdeeabd9108157a77b14c3d494ab60088adfa
toymaj <sermed512@gmail.com>
2023-08-06 23:30:26 +0300

    migrate to aiohttp

--- likeinterface/session.py
+++ likeinterface/session.py
@@ -4,7 +4,7 @@ import asyncio
 from collections import defaultdict
 from typing import TYPE_CHECKING, Any, Dict, Optional, cast
 
-import httpx
+from aiohttp.client import ClientError, ClientSession
 
 from likeinterface.constants import REQUEST_TIMEOUT
 from likeinterface.exceptions import LikeNetworkError
@@ -18,7 +18,7 @@ if TYPE_CHECKING:
 class SessionManager:
     def __init__(
         self,
-        session: Optional[httpx.AsyncClient] = None,
+        session: Optional[ClientSession] = None,
         *,
         connect_kwargs: Dict[str, Any] = defaultdict(),  # noqa
     ) -> None:
@@ -29,20 +29,20 @@ class SessionManager:
     async def create(self) -> None:
         if self.should_reset_connector:
             await self.close()
-        if self.session is None or self.session.is_closed:
-            self.session = httpx.AsyncClient(**self.connect_kwargs)
+        if self.session is None or self.session.closed:
+            self.session = ClientSession(**self.connect_kwargs)
             self.should_reset_connector = False
 
     async def close(self) -> None:
-        if self.session and not self.session.is_closed:
-            await self.session.aclose()
+        if self.session and not self.session.closed:
+            await self.session.close()
 
 
 class Session(SessionManager):
     def __init__(
         self,
         *,
-        session: Optional[httpx.AsyncClient] = None,
+        session: Optional[ClientSession] = None,
         connect_kwargs: Dict[str, Any] = defaultdict(),  # noqa
     ) -> None:
         super(Session, self).__init__(
@@ -65,12 +65,12 @@ class Session(SessionManager):
             )
         except asyncio.TimeoutError:
             raise LikeNetworkError("Exception %s: %s." % (method, "request timeout error"))
-        except httpx.NetworkError as e:
+        except ClientError as e:
             raise LikeNetworkError(
                 "Exception for method %s: %s." % (method.__name__, f"{type(e).__name__}: {e}")
             )
 
         response = response_validator(
-            method=method, status_code=response.status_code, content=response.text
+            method=method, status_code=response.status, content=response.text
         )
         return cast(LikeType, response.result)
